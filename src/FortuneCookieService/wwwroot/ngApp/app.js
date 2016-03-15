176d040a70aeec326f887f6a495ef9fcd77f74ab
Ola Siyanbola <oasiyanbola@gmail.com>
2016-03-15 16:24:31 -0600

    add more angular exerise blinding and handling

--- src/FortuneCookieService/wwwroot/ngApp/app.js
+++ src/FortuneCookieService/wwwroot/ngApp/app.js
@@ -1,1 +1,41 @@
+//Fortune Cookie Service
+//Create a service that randomly returns one of three fortunes (for example, "Your luck is about to change!"). Use service with a controller and view to display the fortune.
+var FortuneCookieService;
+(function (FortuneCookieService) {
+    angular.module("FortuneCookieService", []);
+    var CookieController = (function () {
+        //public fortune;...
+        function CookieController(FortuneService) {
+            this.FortuneService = FortuneService;
+        }
+        Object.defineProperty(CookieController.prototype, "fortune", {
+            //this.fortune = FortuneService.getRamdomFortune();....
+            get: function () {
+                return this.tell = this.FortuneService.fortunes();
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return CookieController;
+    }());
+    angular.module("FortuneCookieService").controller("CookieController", CookieController);
+    var FortuneService = (function () {
+        function FortuneService() {
+            this.fortunes = function () {
+                var fortune = Math.random();
+                if (fortune < 0.33) {
+                    return "Today your luck will change for the better!";
+                }
+                else if (fortune < 0.66) {
+                    return "You might not want to get out of bed today!";
+                }
+                else {
+                    return "You future is cloudy!";
+                }
+            };
+        }
+        return FortuneService;
+    }());
+    angular.module("FortuneCookieService").service("FortuneService", FortuneService);
+})(FortuneCookieService || (FortuneCookieService = {}));
 //# sourceMappingURL=app.js.map
--- src/FortuneCookieService/wwwroot/ngApp/app.ts
+++ src/FortuneCookieService/wwwroot/ngApp/app.ts
@@ -1,1 +1,75 @@
 ﻿
+
+//Fortune Cookie Service
+//Create a service that randomly returns one of three fortunes (for example, "Your luck is about to change!"). Use service with a controller and view to display the fortune.
+
+namespace FortuneCookieService {
+    angular.module("FortuneCookieService", [])
+    class CookieController {
+        public tell;
+    //public fortune;...
+        constructor(private FortuneService: FortuneService) {
+        }
+
+        //this.fortune = FortuneService.getRamdomFortune();....
+
+        get fortune() {
+            return this.tell = this.FortuneService.fortunes();
+        }
+
+      
+
+    }
+    angular.module("FortuneCookieService").controller("CookieController", CookieController);
+
+    class FortuneService {
+
+       fortunes = function() {
+          let  fortune = Math.random();
+            if (fortune < 0.33) {
+                return "Today your luck will change for the better!";
+            } else if (fortune < 0.66) {
+                return "You might not want to get out of bed today!";
+            } else {
+                return "You future is cloudy!";
+            }
+        }
+
+
+
+//Instructors Example..............
+       //FortuneService = [
+       //"Good news",
+       //    "Bad news",
+       //    "Great news"
+       //];
+
+       //getRamdomFortune() {
+       //    return this.fortunes[Math.floor(Math.random() * this.fortunes.length)];
+       //}
+
+
+
+
+
+
+
+}
+    angular.module("FortuneCookieService").service("FortuneService", FortuneService);
+
+
+
+    //class CalculateController {
+
+    //    public sum;
+
+    //    solution = function(num1, num2) {
+    //        return this.sum = num1 + num2;
+    //    }
+
+    //}
+
+    //angular.module("FortuneCookieService").controller("CalculateController", CalculateController);
+
+
+}
